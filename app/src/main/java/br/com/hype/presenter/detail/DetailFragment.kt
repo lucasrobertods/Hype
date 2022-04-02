839ec418dc0298de78289b3fbef36b7eef628d3b
lucasrobertods97 <67114212+lucasrobertods97@users.noreply.github.com>
2022-04-02 15:40:07 -0300

    add event to calendar

--- app/src/main/java/br/com/hype/presenter/detail/DetailFragment.kt
+++ app/src/main/java/br/com/hype/presenter/detail/DetailFragment.kt
@@ -3,6 +3,7 @@ package br.com.hype.presenter.detail
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,13 +11,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import br.com.hype.databinding.FragmentDetailBinding
 import com.bumptech.glide.Glide
+import java.util.*
 
 class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
-    private val args: DetailFragmentArgs by navArgs()
-
     private val binding get() = _binding!!
+
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,12 +28,20 @@ class DetailFragment : Fragment() {
 
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
 
-        setupView()
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
     private fun setupView() {
+        showData()
+        setupListeners()
+    }
+
+    private fun showData() {
         args.event.also { event ->
             Glide.with(requireContext()).load(event.photo).into(binding.imgEvent)
             binding.tvNome.text = event.name
@@ -39,13 +49,44 @@ class DetailFragment : Fragment() {
             binding.tvHour.text = event.hour
             binding.tvLocation.text = event.location
             binding.tvDescription.text = event.description
-
-            binding.btnNavigate.setOnClickListener {
-                val openURL = Intent(Intent.ACTION_VIEW)
-                openURL.data = Uri.parse(event.link)
-                startActivity(openURL)
-            }
         }
+    }
+
+    private fun setupListeners() {
+        binding.btnNavigate.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse(args.event.link)
+            startActivity(openURL)
+        }
+        binding.llReminder.setOnClickListener {
+            addEvent()
+        }
+    }
+
+    private fun addEvent() {
+        val dateSplit = args.event.date.split("/")
+        val day = dateSplit[0].toInt()
+        val month = dateSplit[1].toInt()
+        val year = dateSplit[2].toInt()
+
+        val hourSplit = args.event.hour.split(":")
+        val hour = hourSplit[0].toInt()
+        val minute = hourSplit[1].toInt()
+
+        val beginTime = Calendar.getInstance()
+        beginTime.set(year, month, day, hour, minute)
+
+        val endTime = Calendar.getInstance()
+        endTime.set(year, month, day, hour + 4, 0)
+
+        val intent = Intent(Intent.ACTION_EDIT)
+        intent.type = "vnd.android.cursor.item/event";
+        intent.putExtra(CalendarContract.Events.TITLE, args.event.name)
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.timeInMillis)
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.timeInMillis)
+        intent.putExtra(CalendarContract.Events.ALL_DAY, false)
+        intent.putExtra(CalendarContract.Events.DESCRIPTION, args.event.description)
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
